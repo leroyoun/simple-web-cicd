497d424202f9f1164560c4e07e70bd6e683527b1
leroyoun <leroyoun@users.noreply.github.com>
2026-06-20 13:49:55 +0800

    fix: 重写test_app.py修复编码

--- test_app.py
+++ test_app.py
@@ -1,17 +1,19 @@
 import sys
-sys.path.insert(0,".")
+sys.path.insert(0, ".")
 from app import app
 import unittest
+
 
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
-    
+
     def test_index(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         data = response.data.decode("utf-8")
         self.assertIn("CI", data)
+
 
 if __name__ == "__main__":
     unittest.main()
